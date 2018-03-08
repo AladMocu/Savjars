6577ea90cbc7ce8a8b47c70a5555f624fbbeb3b5
ALBERT ADOLFO MOLANO CUBILLOS <aa.molano@sis.virtual.uniandes.edu.co>
2018-03-08 13:21:18 -0500

    initial commit

---
+++ Assets/scripts/Basics_Movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basics_Movement : MonoBehaviour {
+
+	public KeyCode jump;
+	public KeyCode forward;
+	public KeyCode back;
+	public KeyCode left;
+	public KeyCode rigth;
+
+	public float step;
+	private double jumpSleeper;
+
+	private bool salto;
+	// Use this for initialization
+	void Start () {
+		salto = false;
+		jumpSleeper = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (jump)&&!salto) {
+			GetComponent<Rigidbody> ().AddForce (new Vector3(0,200,0));
+			salto = true;
+
+		}
+		if (Input.GetKey (forward)) {
+			transform.Translate (0,0,-step);
+		}
+		if (Input.GetKey (back)) {
+			transform.Translate (0,0,step);
+		}
+		if (Input.GetKey (left)) {
+			transform.Translate (step,0,0);
+		}
+		if (Input.GetKey (rigth)) {
+			transform.Translate (-step,0,0);
+		}
+		if (salto) {
+			jumpSleeper++;
+		}
+		if (jumpSleeper == 60) {
+			jumpSleeper = 0;
+			salto = false;
+		}
+
+	}
+}
---
+++ Assets/scripts/CameraChange.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChange : MonoBehaviour {
+
+	public Camera camPlayer1;
+	public Camera camPlater2;
+	private int actual;
+	public KeyCode change;
+	bool salto = true;
+	private double jumpSleeper;
+	// Use this for initialization
+	void Start () {
+		actual = 0;
+		salto = false;
+		jumpSleeper = 0;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (change)&&!salto) {
+			actual = actual == 0 ? 1 : 0;
+			salto = true;
+		}
+		camPlayer1.targetDisplay = actual;
+		camPlater2.targetDisplay=actual == 0 ? 1 : 0;
+		if (salto) {
+			jumpSleeper++;
+		}
+		if (jumpSleeper == 60) {
+			jumpSleeper = 0;
+			salto = false;
+		}
+	}
+}
