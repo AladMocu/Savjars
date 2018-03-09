056bd7d54fcdce1c1bf0212bf1c9b6dde24a0219
ALBERT ADOLFO MOLANO CUBILLOS <aa.molano@sis.virtual.uniandes.edu.co>
2018-03-09 13:29:34 -0500

    tutorial base

---
+++ Assets/scripts/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+
+	public Animator animator;
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("player entry button ");
+	}
+	void OnTriggerStay(Collider other)
+	{
+		Debug.Log("player on button ");
+	}
+
+}
