968d2f6c51eb8f928ae607d40ee6a6bd67cae9ea
AladMocu <aladmocu@gmail.com>
2018-03-10 21:26:33 -0500

    Button Working

--- Assets/scripts/Button.cs
+++ Assets/scripts/Button.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Button : MonoBehaviour {
 
+	public GameObject door;
+
 	public Animator animator;
+	protected float massOverButton=0;
+
+	void Start()
+	{
+		animator.enabled = false;
+	}
 
 	void OnTriggerEnter(Collider other)
 	{
-		Debug.Log("player entry button ");
+		massOverButton+=other.GetComponent<Rigidbody> ().mass;
 	}
 	void OnTriggerStay(Collider other)
 	{
-		Debug.Log("player on button ");
+		Debug.Log("mass over button: "+massOverButton);
+		if (massOverButton > 1) {
+			animator.enabled = true;
+		}
+	}
+	void OnTriggerExit(Collider other)
+	{
+		massOverButton-=other.GetComponent<Rigidbody> ().mass;
 	}
 
 }
